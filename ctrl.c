1ced6f06010a63422ae70ffd99d80a34f477d40d
zhc105 <me@zhc105.net>
2021-09-29 13:41:53 +0800

    fix

--- ctrl.c
+++ ctrl.c
@@ -1,4 +1,6 @@
-/* * Copyright (c) 2021, Moonflow <me@zhc105.net> * All rights reserved.
+/*
+ * Copyright (c) 2021, Moonflow <me@zhc105.net>
+ * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
